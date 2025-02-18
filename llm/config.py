b8e69dacb7ee146591208ee6765aefc5726f949f
杜晋华 <dujinhua2021@163.com>
2025-02-18 19:47:55 +0800

    2.18

---
+++ llm/config.py
@@ -0,0 +1,19 @@
+# GPT API密钥和基本URL
+GPT_API_KEY = "sk-WKTbQOVHthXy0OOqAf5f95A70aB54dA18e062aD17a186619"
+GPT_BASE_URL = "https://one-api.glm.ai/v1"
+# GLM API密钥和基本URL
+GLM_API_KEY = "QGqhzEef29eGMA5buGB9JRrUdSweTCMe2Kcg4TGDdjGqwkJFLGW2h56Fccq86rYB"
+GLM_BASE_URL = "https://api.chatglm.cn/v1"
+
+# 新版本chatglm调用方式相关参数
+GLM_NEW_API_KEY = 'ttEwtN5KYDKs97gQsIGEMMdV8TpvwGfPwRDfNPXhtxRh7f7pyfDUYeeXfgLZ7SJ2'
+GLM_NEW_API_URL = "https://117.50.198.214:8443/v1/chat/completions"
+GLM_Authorization = "Bearer" + " " + GLM_NEW_API_KEY
+
+# 用于答案补全的配置参数
+
+TGI_URL = "http://172.18.64.8:8080/generate"
+
+# 用于答案评分的配置参数
+# CRITIC_URL = "http://172.18.64.63:9090/v1/chat/completions"
+CRITIC_URL = "http://172.18.64.55:8080/generate"
